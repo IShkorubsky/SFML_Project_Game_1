e9e2d23a1fa6256539d1cc442146ecc26390b735
Illya Shkorubs'ky <illyashkorubsky@hotmail.com>
2025-01-28 21:20:19 +0000

    changed to red
    Change CircleShape color from green to red
    
    Updated the `sf::CircleShape` object named `shape` to have a red color instead of green.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
     sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    shape.setFillColor(sf::Color::Red);
 
     while (window.isOpen())
     {
